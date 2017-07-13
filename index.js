ad7a67853c323971deeedb96e046d1b79f266c3f
Jonathan Sax <jsax8310@outlook.com>
2017-07-13 14:23:35 -0600

    finished calculator

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var express = require(‘express’);
+var express = require('express');
 var server = express();
 var port = 8080;
 
-server.get(‘/math/pi’,function(req,res){
+server.get('/math/pi',function(req, res){
   res.send(`${Math.PI}`);
+})
+
+server.get('/math/calculate', function(req, res) {
+  var oper = req.query.operation
+  var x = parseFloat(req.query.x)
+  var y = parseFloat(req.query.y)
+  if (oper === 'add'){
+    res.send(`${x + y}`)
+  } else if (oper === 'subtract') {
+    res.send(`${x - y}`)
+  } else if (oper === 'divide') {
+    res.send(`${x / y}`)
+  } else if (oper === 'multiply') {
+    res.send(`${x * y}`)
+  } else {
+    res.send(`${x + y}`)
+  }
+
 })
 
 server.listen(port);
