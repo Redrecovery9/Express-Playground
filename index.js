5bbd0915f51ec5863b996f226bcd3e77dc930469
Jonathan Sax <jsax8310@outlook.com>
2017-07-14 09:43:30 -0600

    finished the volume test

--- index.js
+++ index.js
@@ -21,7 +21,22 @@ server.get('/math/calculate', function(req, res) {
   } else {
     res.send(`${x + y}`)
   }
+})
 
+server.post('/math/sum', function(req, res) {
+  var n = req.query.n
+  var result = 0
+  for (var i = 0; i < n.length; i++) {
+  result += parseFloat(n[i])
+  }
+  res.send(String(result))
+})
+
+server.patch('/math/volume/:num1/:num2/:num3', function(req, res) {
+  var number1 = parseFloat(req.params.num1)
+  var number2 = parseFloat(req.params.num2)
+  var number3 = parseFloat(req.params.num3)
+  res.send(String(number1 * number2 * number3))
 })
 
 server.listen(port);
